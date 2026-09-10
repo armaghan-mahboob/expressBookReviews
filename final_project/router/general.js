2c93de671abf54f3fffcf3338ac5b021aa68c90a
ManiTheLegend <cute.armanijeans@gmail.com>
2026-09-10 15:26:46 +0500

    Task 3 done

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -27,8 +27,21 @@ public_users.get("/isbn/:isbn", function (req, res) {
 
 // Get book details based on author
 public_users.get("/author/:author", function (req, res) {
-  //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  const authorParam = req.params.author.toLowerCase();
+  const bookKeys = Object.keys(books);
+  const matchingBooks = [];
+
+  bookKeys.forEach((key) => {
+    if (books[key].author.toLowerCase() === authorParam) {
+      matchingBooks.push({ isbn: key, ...books[key] });
+    }
+  });
+
+  if (matchingBooks.length > 0) {
+    return res.status(200).json(matchingBooks);
+  } else {
+    return res.status(404).json({ message: "No books found for this author" });
+  }
 });
 
 // Get all books based on title
