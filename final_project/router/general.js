fc38769b27340f9eb05e6f1e45df2880d63c3cc4
ManiTheLegend <cute.armanijeans@gmail.com>
2026-09-10 15:45:37 +0500

    Implement Tasks 10-13 using Promises and Async-Await with Axios

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,10 +1,10 @@
 const express = require("express");
+const axios = require("axios");
 let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
-// Task 6: Register a new user
 public_users.post("/register", (req, res) => {
   const username = req.body.username;
   const password = req.body.password;
@@ -27,61 +27,109 @@ public_users.post("/register", (req, res) => {
     .json({ message: "User successfully registered. Now you can login" });
 });
 
-// Get the book list available in the shop
-public_users.get("/", function (req, res) {
-  return res.send(JSON.stringify(books, null, 4));
+// Helper function to return local books as a Promise
+const getBooksAsync = () => {
+  return new Promise((resolve) => {
+    resolve(books);
+  });
+};
+
+// Task 10: Get all books using Async-Await / Promises
+public_users.get("/", async function (req, res) {
+  try {
+    const bookList = await getBooksAsync();
+    return res.status(200).send(JSON.stringify(bookList, null, 4));
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "Error fetching books list", error: error.message });
+  }
 });
 
-// Get book details based on ISBN
+// Task 11: Get book details based on ISBN using Promises
 public_users.get("/isbn/:isbn", function (req, res) {
   const isbn = req.params.isbn;
 
-  if (books[isbn]) {
-    return res.status(200).json(books[isbn]);
-  } else {
-    return res.status(404).json({ message: "Book not found" });
-  }
+  getBooksAsync()
+    .then((bookList) => {
+      if (bookList[isbn]) {
+        return res.status(200).json(bookList[isbn]);
+      } else {
+        return res.status(404).json({ message: "Book not found" });
+      }
+    })
+    .catch((error) => {
+      return res
+        .status(500)
+        .json({ message: "Error fetching book details", error: error.message });
+    });
 });
 
-// Get book details based on author
-public_users.get("/author/:author", function (req, res) {
-  const authorParam = req.params.author.toLowerCase();
-  const bookKeys = Object.keys(books);
-  const matchingBooks = [];
+// Task 12: Get book details based on Author using Async-Await
+public_users.get("/author/:author", async function (req, res) {
+  try {
+    const authorParam = req.params.author.toLowerCase();
+    const bookList = await getBooksAsync();
+    const bookKeys = Object.keys(bookList);
+    const matchingBooks = [];
 
-  bookKeys.forEach((key) => {
-    if (books[key].author.toLowerCase() === authorParam) {
-      matchingBooks.push({ isbn: key, ...books[key] });
+    bookKeys.forEach((key) => {
+      if (bookList[key].author.toLowerCase() === authorParam) {
+        matchingBooks.push({ isbn: key, ...bookList[key] });
+      }
+    });
+
+    if (matchingBooks.length > 0) {
+      return res.status(200).json(matchingBooks);
+    } else {
+      return res
+        .status(404)
+        .json({ message: "No books found for this author" });
     }
-  });
-
-  if (matchingBooks.length > 0) {
-    return res.status(200).json(matchingBooks);
-  } else {
-    return res.status(404).json({ message: "No books found for this author" });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({
+        message: "Error fetching books by author",
+        error: error.message,
+      });
   }
 });
 
-// Task 4: Get all books based on title
+// Task 13: Get book details based on Title using Promises
 public_users.get("/title/:title", function (req, res) {
   const titleParam = req.params.title.toLowerCase();
-  const bookKeys = Object.keys(books);
-  const matchingBooks = [];
 
-  bookKeys.forEach((key) => {
-    if (books[key].title.toLowerCase() === titleParam) {
-      matchingBooks.push({ isbn: key, ...books[key] });
-    }
-  });
+  getBooksAsync()
+    .then((bookList) => {
+      const bookKeys = Object.keys(bookList);
+      const matchingBooks = [];
 
-  if (matchingBooks.length > 0) {
-    return res.status(200).json(matchingBooks);
-  } else {
-    return res.status(404).json({ message: "No books found with this title" });
-  }
+      bookKeys.forEach((key) => {
+        if (bookList[key].title.toLowerCase() === titleParam) {
+          matchingBooks.push({ isbn: key, ...bookList[key] });
+        }
+      });
+
+      if (matchingBooks.length > 0) {
+        return res.status(200).json(matchingBooks);
+      } else {
+        return res
+          .status(404)
+          .json({ message: "No books found with this title" });
+      }
+    })
+    .catch((error) => {
+      return res
+        .status(500)
+        .json({
+          message: "Error fetching books by title",
+          error: error.message,
+        });
+    });
 });
 
-// Task 5: Get book review
+// Get book review
 public_users.get("/review/:isbn", function (req, res) {
   const isbn = req.params.isbn;
 
